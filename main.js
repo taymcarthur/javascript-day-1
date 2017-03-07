dd0758e5e7a1780db935cbe49dcc97110f31c869
Taylor McArthur <taydm.erek@gmail.com>
2017-03-07 08:54:47 -0800

    finished almost

--- main.js
+++ main.js
@@ -1,6 +1,40 @@
-function myfunction() {
-	alert("Welcome to my page!");
-}
+alert("Welcome to my page!");
+
 var name = prompt('What is your name?');
-alert(”Hi “ + name + “! Let’s get started”)
+alert("Hi " + name + "! Let’s get started")
+
+var play = confirm("Do you really want to play a game?");
+    if (play == true){
+        alert("Let's Play");
+    } else{
+        alert("So Rude!!!");
+    }
+var q1 = confirm('Is your flavorite candybar Snickers?');
+	if (q1 == true) { 
+		alert("so yum!")
+	}	else{
+		alert("What's wrong with you?")
+		alert("GAME OVER")
+	}
+
+var q2 = confirm('Does this course make you feel like you are slowly dying?');
+	if (q2 == true) { 
+		alert("Same")
+	}	else{
+		alert("You're not doing it right")
+		alert("GAME OVER")
+	}
+
+var q3 = confirm('is taylor your favorite student?')
+	if (q3 == true) {
+		alert("YOU WIN!")
+	}	else{
+		alert("YOU SUCK")
+		alert("GAME OVER")
+	}
+
+
+
+
+
 
